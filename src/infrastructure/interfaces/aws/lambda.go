7648249dd628417ac70c4c5657321c8f2f8fd710
Carlos Romero <carlos.romero@99minutos.com>
2023-11-11 20:43:16 -0600

    Added logger in the aws lambda request

--- src/infrastructure/interfaces/aws/lambda.go
+++ src/infrastructure/interfaces/aws/lambda.go
@@ -5,9 +5,14 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/karlozz157/storicard/src/utils"
 )
 
+var logger = utils.GetLogger()
+
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	logger.Info("request", request)
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "hello world",
